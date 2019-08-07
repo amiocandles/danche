aeb557d25a629ae742bd454161bd1b360e9e453b
yield <3502746856@qq.com>
2019-08-07 18:22:56 +0800

    '项目架构'

--- imoocmanger/src/pages/demo/Life.js
+++ imoocmanger/src/pages/demo/Life.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Child from './Chile.js'
 //按需导入antDesign组件
-//import { Button } from 'antd'
+import { Button,Input } from 'antd'
 //导入样式
-//import 'antd/dist/antd.css'
+// import 'antd/dist/antd.css'
 import './index.less'
 
 export default class Life extends React.Component{
@@ -19,16 +19,10 @@ export default class Life extends React.Component{
         })
     }
     render(){
-        let style={
-            padding:50
-        }
         return <div className="content">
-            {/*<Button ghost>Ghost</Button>*/}
-            {/*<Button ghost disabled>*/}
-            {/*    Ghost(disabled)*/}
-            {/*</Button>*/}
             <p>生命周期</p>
-            {/*<Button type="primary" ghost onClick={this.handeAdd}>AntD点击一下</Button>*/}
+            <Input placeholder="Basic usage" />
+            <Button type="primary" ghost onClick={this.handeAdd}>AntD点击一下</Button>
             <button onClick={this.handeAdd}>点击一下</button>
             <p>{this.state.count}</p>
             <Child name="Bob"></Child>
