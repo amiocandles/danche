714ae80473221621b3e72e04bd767ff2bb1ba693
yield <3502746856@qq.com>
2019-08-08 18:42:36 +0800

    '调用天气API'

--- imoocmanger/src/components/Header/index.js
+++ imoocmanger/src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Row, Col } from 'antd'
 import './header.css'
 import Util from '../../utils/utils.js'
+import axios from '../../axios/common.js'
 export default class Header extends React.Component{
     state={}
     componentWillMount() {
@@ -14,8 +15,22 @@ export default class Header extends React.Component{
                 sysTime
             })
         },1000)
+        this.getWeatherAPIData();
     }
-
+    getWeatherAPIData(){
+        let city='洛阳'
+        axios.jsonp({
+            url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
+        }).then((res)=>{
+            if(res.status == 'success'){
+                let data = res.results[0].weather_data[0];
+                this.setState({
+                    dayPictureUrl:data.dayPictureUrl,
+                    weather:data.weather
+                })
+            }
+        })
+    }
     render() {
         return (
             <div className="header">
@@ -31,7 +46,12 @@ export default class Header extends React.Component{
                     </Col>
                     <Col span={20} className="weather">
                         <span className="data">{this.state.sysTime}</span>
-                        <span className="weather-detail">晴转多云</span>
+                        <span className="weather-img">
+                            <img src={this.state.dayPictureUrl} alt=""/>
+                        </span>
+                        <span className="weather-detail">
+                             {this.state.weather}
+                        </span>
                     </Col>
                 </Row>
             </div>
