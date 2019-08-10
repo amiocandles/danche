aa44f003f978c580d0a8641baa59a1baff9f62ff
yield <3502746856@qq.com>
2019-08-10 10:53:05 +0800

    'ui的按钮组件页面'

--- imoocmanger/src/router.js
+++ imoocmanger/src/router.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { HashRouter, Route } from 'react-router-dom'
+import { HashRouter, Route,Switch } from 'react-router-dom'
 import App from './App.js'
 import Login from './pages/login'
 import Admin from './admin'
 import Buttons from './pages/ui/buttons'
 import NoMatch from "./pages/nomatch";
+// import {Switch} from "antd";
 export default class IRouter extends React.Component {
     render(){
         return(
@@ -13,8 +14,10 @@ export default class IRouter extends React.Component {
                     <Route path="/login" component={Login}/>
                     <Route path="/admin" render={()=>
                         <Admin>
+                            <Switch>
                             <Route path="/admin/ui/buttons" component={Buttons}/>
                             <Route component={NoMatch}/>
+                            </Switch>
                         </Admin>
                     }/>
                     <Route path="/order/detail" component={Login}/>
