c1e436988bd0e1d413d410d9d5636ad3126b9c25
yield <3502746856@qq.com>
2019-08-12 11:38:46 +0800

    '轮播'

--- imoocmanger/src/router.js
+++ imoocmanger/src/router.js
@@ -11,6 +11,7 @@ import Notice from './pages/ui/notice'
 import Messages from './pages/ui/messages'
 import Tabs from './pages/ui/tabs'
 import Gallery from './pages/ui/gallery'
+import Carousel from './pages/ui/carousel'
 // import {Switch} from "antd";
 export default class IRouter extends React.Component {
     render(){
@@ -28,6 +29,7 @@ export default class IRouter extends React.Component {
                             <Route path="/admin/ui/messages" component={Messages}/>
                             <Route path="/admin/ui/tabs" component={Tabs}/>
                             <Route path="/admin/ui/gallery" component={Gallery}/>
+                            <Route path="/admin/ui/carousel" component={Carousel}/>
                             <Route component={NoMatch}/>
                             </Switch>
                         </Admin>
