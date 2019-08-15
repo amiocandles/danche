e73b83593b3d067a7dafcee4e4214854d5bfbec7
yield <3502746856@qq.com>
2019-08-15 14:49:59 +0800

    '模块化封装-BaseForm‘

--- imoocmanger/src/pages/order/index.js
+++ imoocmanger/src/pages/order/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Card, Button, Table, Form, Select, Modal, DatePicker, message} from 'antd'
 import axios from '../../axios/common'
 import Utils from '../../utils/utils'
-//import BaseForm from '../../components/BaseForm'
+import BaseForm from '../../components/BaseForm'
 const FormItem = Form.Item;
 const Option = Select.Option;
 export default class Order extends React.Component{
@@ -192,9 +192,9 @@ export default class Order extends React.Component{
         }
         return (
             <div>
-                {/*<Card>*/}
-                {/*    <BaseForm formList={this.formList} filterSubmit={this.handleFilter}/>*/}
-                {/*</Card>*/}
+                <Card>
+                    <BaseForm formList={this.formList} filterSubmit={this.handleFilter}/>
+                </Card>
                 <Card style={{marginTop:10}}>
                     <Button type="primary" onClick={this.openOrderDetail}>订单详情</Button>
                     <Button type="primary" style={{marginLeft:10}} onClick={this.handleConfirm}>结束订单</Button>
