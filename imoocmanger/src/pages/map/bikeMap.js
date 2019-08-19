57aa091c56141fa4de2357e0d65e4295a9646588
yield <3502746856@qq.com>
2019-08-19 09:53:06 +0800

    '地图数据渲染实现'

--- imoocmanger/src/pages/map/bikeMap.js
+++ imoocmanger/src/pages/map/bikeMap.js
@@ -57,6 +57,72 @@ export default class Order extends React.Component{
         let gps2 = list[list.length - 1].split(',');
         let endPoint = new window.BMap.Point(gps2[0], gps2[1]);
         this.map.centerAndZoom(endPoint, 11);
+        //添加起始图标
+        let startPointIcon = new window.BMap.Icon("/assets/start_point.png", new window.BMap.Size(36, 42), {
+            imageSize: new window.BMap.Size(36, 42),
+            anchor: new window.BMap.Size(18, 42)//偏移量
+        });
+        var bikeMarkerStart = new window.BMap.Marker(startPoint, { icon: startPointIcon });
+        this.map.addOverlay(bikeMarkerStart);
+        let endPointIcon = new window.BMap.Icon("/assets/end_point.png", new window.BMap.Size(36, 42), {
+            imageSize: new window.BMap.Size(36, 42),
+            anchor: new window.BMap.Size(18, 42)//偏移量
+        });
+        var bikeMarkerEnd = new window.BMap.Marker(endPoint, { icon: endPointIcon });
+        this.map.addOverlay(bikeMarkerEnd);
+        //绘制车辆行驶路线
+        let routeList = [];
+        list.forEach((item)=>{//item是字符串split分隔
+            let p = item.split(",");
+            //let point = new window.BMap.Point(p[0], p[1]);
+            routeList.push(new window.BMap.Point(p[0], p[1]));
+        })
+        // 行驶路线
+        var polyLine = new window.BMap.Polyline(routeList, {//polyLine折现
+            strokeColor: "#ef4136",
+            strokeWeight: 3,
+            strokeOpacity: 1
+        });
+        this.map.addOverlay(polyLine);
+        // 服务区路线
+        let serviceList = res.service_list;
+        let servicePointist = [];
+        serviceList.forEach((item) => {
+            // let point = new window.BMap.Point(item.lon, item.lat);
+            servicePointist.push(new window.BMap.Point(item.lon, item.lat));
+        })
+        // 画线
+        var polyServiceLine = new window.BMap.Polyline(servicePointist, {
+            strokeColor: "#ef4136",
+            strokeWeight: 3,
+            strokeOpacity: 1
+        });
+        this.map.addOverlay(polyServiceLine);
+        // 添加地图中的自行车
+        let bikeList = res.bike_list;
+        let bikeIcon = new window.BMap.Icon("/assets/bike.jpg", new window.BMap.Size(36, 42), {
+            imageSize: new window.BMap.Size(36, 42),
+            anchor: new window.BMap.Size(18, 42)
+        });
+        bikeList.forEach((item) => {
+            let p = item.split(",");
+            let point = new window.BMap.Point(p[0], p[1]);
+            var bikeMarker = new window.BMap.Marker(point, { icon: bikeIcon });
+            this.map.addOverlay(bikeMarker);
+        })
+        // 添加地图控件
+        this.addMapControl();
+    }
+    // 添加地图控件
+    addMapControl = ()=>{
+        let map = this.map;
+        // 左上角，添加比例尺
+        var top_right_control = new window.BMap.ScaleControl({anchor: window.BMAP_ANCHOR_TOP_RIGHT});
+        var top_right_navigation = new window.BMap.NavigationControl({anchor: window.BMAP_ANCHOR_TOP_RIGHT});
+        //添加控件和比例尺
+        map.addControl(top_right_control);
+        map.addControl(top_right_navigation);
+        map.enableScrollWheelZoom(true);
     }
 
     render() {
