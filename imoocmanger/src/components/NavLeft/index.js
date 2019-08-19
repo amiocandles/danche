e6332706ac8a4fad58b9439f8d030940da6bec40
yield <3502746856@qq.com>
2019-08-19 19:41:34 +0800

    'redux实战开发'

--- imoocmanger/src/components/NavLeft/index.js
+++ imoocmanger/src/components/NavLeft/index.js
@@ -2,13 +2,28 @@ import React from 'react'
 //antDesign组件
 import { Menu } from 'antd';
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { switchMenu, saveBtnList } from './../../redux/action'
 import MenuConfig from './../../config/menuConfig.js'
 import './left.css'
-const { SubMenu } = Menu;
+// const { SubMenu } = Menu;
+const SubMenu = Menu.SubMenu;
+class NavLeft extends React.Component{
 
-export default class NavLeft extends React.Component{
+    state = {
+        currentKey: ''
+    }
+    handleClick = ({ item, key }) =>{
+        // 事件派发，自动调用reducer，通过reducer保存到store对象中
+        const { dispatch } = this.props;
+        dispatch(switchMenu(item.props.title));
+        this.setState({
+            currentKey: key
+        });
+    }
     componentWillMount() {
         const menuTreeNode = this.renderMenu(MenuConfig);
+        let currentKey = window.location.hash.replace(/#|\?.*$/g,'');
         this.setState({
             menuTreeNode
         })
@@ -34,10 +49,14 @@ export default class NavLeft extends React.Component{
                 <img  src="/assets/logo.png" alt=""/>
                 <h1>Imooc MS</h1>
             </div>
-            <Menu theme="dark">
+            <Menu
+                theme="dark"
+                onClick={this.handleClick}
+                selectedKeys={this.state.currentKey}
+            >
                 {this.state.menuTreeNode}
             </Menu>
-
         </div>);
     }
 }
+export default connect()(NavLeft);
